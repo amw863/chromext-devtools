00bfdcbc7c18ec622a8f0a10a69dabe0f61c7fb1
test <test@example.com>
2023-10-15 18:32:37 +0800

    添加定时任务和通知

--- service-worker.js
+++ service-worker.js
@@ -38,13 +38,54 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 );
 
-
-
 function download(url) {
     var options={
         url:url
     }
 
-
     chrome.downloads.download(options);
+}
+
+// 定时任务
+console.log("设置定时任务");
+chrome.alarms.create('demo-default-alarm', {
+  delayInMinutes: 1,
+  periodInMinutes: 1
+});
+
+chrome.alarms.onAlarm.addListener((info) => {
+  console.log(info);
+  sendNotification();
+});
+console.log("设置定时任务完成");
+
+
+// 通知消息
+// chrome.notifications.onClicked.addListener((notificationId) => {
+//   // 当用户点击通知时触发
+//   console.log('用户点击了通知');
+// });
+
+// chrome.action.onClicked.addListener(() => {
+//   // 当用户点击扩展图标时触发
+//   console.log('用户点击了扩展图标');
+//   sendNotification();
+// });
+// sendNotification();
+
+function sendNotification() {
+  const options = {
+    type: 'basic',
+    iconUrl: './icon.png',
+    title: '新消息提醒',
+    message: '您有一条新消息'
+  };
+
+  // 此处有坑啊,id 不为空时只推送一次，id为空推送
+  // https://stackoverflow.com/questions/27315302/chrome-extension-notification-shows-only-once
+  // 修改service-work需要重新加载扩展
+  // 通知需要浏览器开启通知
+  chrome.notifications.create('', options, (notificationId) => {
+    console.log('通知已发送');
+  });
 }
